71949f913414a62aac749b97b8274dead323c650
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2022-11-08 23:38:56 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/agnaldo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/agnaldo/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.agnaldo.helpdesk.domain.Tecnico;
 import com.agnaldo.helpdesk.repositories.TecnicoRepository;
+import com.agnaldo.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id ));
 	}
 	
 }
