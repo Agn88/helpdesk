eefd67e54459d5dc8558c03d223d57e4e22387b2
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2022-11-21 21:14:23 -0300

    Validação nos campos de criação de novo usuário

--- src/main/java/com/agnaldo/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/agnaldo/helpdesk/repositories/PessoaRepository.java
@@ -1,10 +1,13 @@
 package com.agnaldo.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.agnaldo.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
 
-	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
